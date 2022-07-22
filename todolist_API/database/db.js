86f90e303b77ab967231c8c962c1cebf8f4ebd1c
bigjihua <bigjihua@Gmail.com>
2022-07-22 16:47:07 +0800

    7-21

--- todolist_API/database/db.js
+++ todolist_API/database/db.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-
+    host:'43.129.172.233',
+    user:'todolist_vue2',
+    password:'todolist_vue2',
+    database:'todolist_vue2'
 })
 
 module.exports = db
